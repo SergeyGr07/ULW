b32ea69ffc93d4db147b98d0c18037051785ef28
SergeyGr07 <49278850+SergeyGr07@users.noreply.github.com>
2023-03-02 09:52:26 +0800

    first commit

---
+++ tweeks_rec.py
@@ -0,0 +1,17 @@
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Load the .wav file
+y, sr = librosa.load('Whistlers.wav')
+
+# Compute power spectrogram
+D = librosa.amplitude_to_db(librosa.stft(y), ref=np.max)
+
+# Display the spectrogram
+plt.figure(figsize=(10, 5))
+librosa.display.specshow(D, y_axis='linear', x_axis='time', cmap='inferno')
+plt.colorbar(format='%+5.0f dB')
+plt.title('Power spectrogram')
+plt.show()
